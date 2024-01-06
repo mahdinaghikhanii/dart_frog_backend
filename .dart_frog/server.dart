103f96a0d9e099c17116d75249992af7306f0b0d
Mahdinkh7 <mahdinkh7@gmail.com>
2024-01-06 23:01:58 +0330

    first commit

---
+++ .dart_frog/server.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+
+import '../routes/index.dart' as index;
+
+
+void main() => hotReload(createServer);
+
+Future<HttpServer> createServer() {
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final handler = Cascade().add(buildRootHandler()).handler;
+  return serve(handler, ip, port);
+}
+
+Handler buildRootHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..mount('/', (context) => buildHandler()(context));
+  return pipeline.addHandler(router);
+}
+
+Handler buildHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..all('/', (context) => index.onRequest(context,));
+  return pipeline.addHandler(router);
+}
---
+++ build/bin/server.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+import '../routes/index.dart' as index;
+
+void main() => createServer();
+
+Future<HttpServer> createServer() async {
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final handler = Cascade().add(buildRootHandler()).handler;
+  final server = await serve(handler, ip, port);
+  print(
+      '\x1B[92m✓\x1B[0m Running on http://${server.address.host}:${server.port}');
+  return server;
+}
+
+Handler buildRootHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()..mount('/', (context) => buildHandler()(context));
+  return pipeline.addHandler(router);
+}
+
+Handler buildHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..all(
+        '/',
+        (context) => index.onRequest(
+              context,
+            ));
+  return pipeline.addHandler(router);
+}
